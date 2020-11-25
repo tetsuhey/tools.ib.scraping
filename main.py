75140405a79aca9a1d7615cd9417967388969f52
Okada Tetsuhei <tetsuhei.okada.8920@gmail.com>
2020-11-25 22:07:22 +0900

    my first push

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+
+
+driver = webdriver.Chrome(executable_path='/Users/okadatetsuhei/Documents/workspace/mylocaldev/tools.ib.scraping/chromedriver')
+driver.get('https://www.google.com/')
+time.sleep(5)
+search_box = driver.find_element_by_name("q")
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
